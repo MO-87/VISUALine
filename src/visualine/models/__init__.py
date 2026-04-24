548398e9c54a6cf2d76f120418da0acf1e632773
MOhamed Abdallah <mo.abdulla632@gmail.com>
2026-04-24 05:58:03 +0300

    inits for PyInstaller packaging

--- src/visualine/models/__init__.py
+++ src/visualine/models/__init__.py
@@ -1,1 +1,5 @@
-"""Initializes the Python package, enabling modules to be imported."""
+from .archs import colorization_wrapper
+from .archs import realesrgan_wrapper
+from .archs import scunet_wrapper
+from .archs import span_wrapper
+from .archs import dis_wrapper
--- src/visualine/nodes/__init__.py
+++ src/visualine/nodes/__init__.py
@@ -1,1 +1,16 @@
-"""Initializes the Python package, enabling modules to be imported."""
+# Utils
+from .utils import grayscale_node
+
+# Upscale (add your others as you build them)
+from .upscale import realesrgan_node
+from .upscale import span_node
+
+# Colorize
+from .colorize import colorization_node
+from .colorize import temporal_prop_node
+
+# Denoise
+from .denoise import scunet_node
+
+# Interpolation
+from .interpolation import rife_node

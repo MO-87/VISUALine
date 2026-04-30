998f8b1d9241551f5cb0dfabf341bcb8404e0817
MOhamed Abdallah <mo.abdulla632@gmail.com>
2026-04-30 07:46:50 +0300

    refine: enhance RIFENode for improved frame interpolation and state management

--- src/visualine/nodes/__init__.py
+++ src/visualine/nodes/__init__.py
@@ -1,16 +1,16 @@
 # Utils
 from .utils import grayscale_node
+from .utils import blur_node
+from .utils import box_utils
+from .utils import box_redaction_node
+from .utils import smart_reframe_node
+from .utils import soft_box_mask_node
+from .utils import solid_background_node
+from .utils import remove_background_node
+from .utils import video_enhance_node
 
-# Upscale (add your others as you build them)
-from .upscale import realesrgan_node
+# Upscale
 from .upscale import span_node
-
-# Colorize
-from .colorize import colorization_node
-from .colorize import temporal_prop_node
-
-# Denoise
-from .denoise import scunet_node
 
 # Interpolation
 from .interpolation import rife_node
